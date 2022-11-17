ac9183386efbdff2083e931c612324f2f18050a9
gavintobin <gavintobin714@gmail.com>
2022-11-17 11:22:26 -0800

    pushpallupdate

--- monty_op_1.c
+++ monty_op_1.c
@@ -3,6 +3,26 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
+	stack_t *nn;
+
+	nn = malloc(sizeof(stack_t));
+
+	if (!nn)
+	{
+		printf("L%d: usage: push integer\n");
+		exit(EXIT_FAILURE);
+	}
+	(void)line_number;
+
+	if (*stack)
+		(*stack)->prev = nn;
+
+	nn->prev = NULL;
+	nn->next = stack;
+	nn->n = 0;
+	*stack = nn;
+}
+	
 void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
@@ -20,3 +40,18 @@ void pop(stack_t **stack, unsigned int line_number)
 		printf("L%d: can't pop an empty stack\n", line_number);
 	exit(EXIT_FAILURE);
 }
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+
+	(void)line_number;
+	temp = *stack;
+	while (temp)
+	{
+		printf("d\n", temp->n);
+		temp = temp->next;
+	}
+}
+
+
