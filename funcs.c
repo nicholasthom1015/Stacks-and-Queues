d58077e3fbb8b028557f28f381b5a95aa334273f
gavintobin <gavintobin714@gmail.com>
2022-11-17 12:17:13 -0800

    added main

--- funcs.c
+++ funcs.c
@@ -52,3 +52,55 @@ void parse(char *c, stack_t **stack, unsigned int line_number)
 		freelist(stack);
 	exit(EXIT_FAILURE);
 }
+/**
+* main - main function of monty interpreter
+* @argv: list of arguments
+* @argc: argument count
+*
+* Return: nothing
+*/
+
+int main(int argc, char *argv[])
+{
+	FILE *rmonty;
+	char *c, buff[1024];
+	unsigned int line_number = 1;
+	stack_t **stack = malloc(sizeof(stack_t *));
+	*stack = NULL;
+
+	if (stack == NULL)
+	{fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	if (argc != 2)
+	{fprintf(stderr, "USAGE: monty file\n");
+		free(stack);
+		exit(EXIT_FAILURE);
+	}
+	if (access(argv[1], R_OK) == -1)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		free(stack);
+		exit(EXIT_FAILURE);
+	}
+	rmonty = fopen(argv[1], "r");
+	if (rmonty == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free(stack);
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		c = fgets(buff, 255, (FILE *)rmonty);
+		if (c == NULL)
+			break;
+		if (whitespace_check(c) != 0)
+			parse(c, stack, line_number);
+		line_number++;
+	}
+	freelist(stack);
+	fclose(rmonty);
+	return (0);
+}
+
