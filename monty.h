d8d4854bd0592883f11b33d19cb6ec2220d14fa8
nicholasthom1015 <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-11-17 10:03:43 -0800

    Update 2 to monty.h

--- monty.h
+++ monty.h
@@ -32,7 +32,7 @@ typedef struct stack_s
 typedef struct instruction_s
 {
 	char *opcode;
-	void (*f)(stack_t **stack, unisgned int line_number);
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 void push(int x);
